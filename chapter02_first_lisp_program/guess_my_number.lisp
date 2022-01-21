65d99681419c0c901b10db9b617a029204cdac9a
Michael Espeña <michael.esp42@protonmail.com>
2022-01-21 15:48:40 +0800

    added some not so useful comments

--- chapter02_first_lisp_program/guess_my_number.lisp
+++ chapter02_first_lisp_program/guess_my_number.lisp
@@ -1,19 +1,41 @@
-; Defining global variables
+;; define starting lower and upper limit
+;; to 1 and 100, respectively
 (defparameter *small* 1)
 (defparameter *big* 100)
 
+
+;; define `guess-my-number` function
+;; that takes no parameter and halves
+;; the sum of `small` and `big` using
+;; the builtin `ash` operator which is
+;; the same with the right and left
+;; binary shift operators you see in
+;; most programming languages
 (defun guess-my-number ()
   (ash (+ *small* *big*) -1))
 
+
+;; define `smaller` function that
+;; decreases the upper limit by one 
+;; and then calls `guess-my-number`
 (defun smaller ()
   (setf *big* (1- (guess-my-number)))
   (guess-my-number))
 
+
+;; define `bigger` function that
+;; increases the lower limit by one 
+;; and then calls `guess-my-number`
 (defun bigger ()
   (setf *small* (1+ (guess-my-number)))
   (guess-my-number))
 
-(defun startover()
+
+;; define `start-over` function
+;; that resets the upper and lower limit
+;; to their original values and calls
+;; `guess-my-number`
+(defun start-over()
   (defparameter *small* 1)
   (defparameter *big* 100)
   (guess-my-number))
