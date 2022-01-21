7970e33d238970c511ec663f9702ee11c896cfce
Michael Espeña <michael.esp42@protonmail.com>
2022-01-21 12:16:25 +0800

    rename folder to include chapter number

---
+++ chapter02_first_lisp_program/guess_my_number.lisp
@@ -0,0 +1,19 @@
+; Defining global variables
+(defparameter *small* 1)
+(defparameter *big* 100)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun startover()
+  (defparameter *small* 1)
+  (defparameter *big* 100)
+  (guess-my-number))
